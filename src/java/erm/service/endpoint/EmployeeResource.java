95350fffdd3d5f9ff8ca48f1c14021e5c69c3cfd
da101025 <daniel.p.jones@gmail.com>
2014-03-29 16:22:39 -0600

    EmployeeResource now includes POST, PUT, and DELETE methods

--- src/java/erm/service/endpoint/EmployeeResource.java
+++ src/java/erm/service/endpoint/EmployeeResource.java
@@ -3,7 +3,11 @@ package erm.service.endpoint;
 import erm.model.business.manager.RosterManager;
 import erm.model.domain.Employee;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,6 +18,10 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 
 /**
  * RESTful call for employee resource
+ * all GET methods in this service should return json or xml depending
+ * on the application type sent in the header request
+ * i.e. for json
+ * curl --header "Accept: application/json" http://localhost:8080/erm/rest/employees
  * @version 1.0
  * @author danieljones
  */
@@ -25,7 +33,10 @@ public class EmployeeResource {
     
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Path("{id}")
+    @Path("{id: \\d+}")
+    /**
+     * Only accepts id with digits between 0 and 9
+     */
     public Response getEmployee(@PathParam("id") int id) {
         
         ResponseBuilder builder = Response.ok(rMgr.getEmployee(id)); //id
@@ -42,6 +53,91 @@ public class EmployeeResource {
         ResponseBuilder builder = Response.ok(entity);
         
         return builder.build();
+        
+    }
+    
+    @POST
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("/create")
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response create(Employee employee) {
+        
+        String msg;
+        if((employee.getFirstName() == null || 
+            employee.getLastName() == null) || 
+            employee.getManager() == null) {
+            
+            msg = "Employee not passed in request";   
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity(msg).
+                    type(MediaType.TEXT_PLAIN).
+                    build();
+            
+        } else {
+            
+            long empId = rMgr.createEmployee(employee);
+            msg = "New employee: '" + employee.getFirstName() + 
+                  " " + employee.getLastName() +
+                  "' created with manager: '" + employee.getManager() +
+                  "' and id: '" + empId + "'";
+            
+        }
+        
+        return Response.ok(msg, MediaType.TEXT_PLAIN).build();
+        
+    }
+    
+    @PUT
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("/{firstName}/{lastName}/{manager}")
+    public Response update(@PathParam("firstName") String firstName,
+                           @PathParam("lastName") String lastName,
+                           @PathParam("manager") String manager) {
+        
+        String msg;
+        if((firstName == null || lastName == null) || manager == null) {
+            
+            msg = "Employee not passed in request";   
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity(msg).
+                    type(MediaType.TEXT_PLAIN).
+                    build();
+            
+        } else {
+            
+            Employee emp = new Employee(firstName,lastName,manager);
+            boolean result = rMgr.saveEmployee(emp);
+            msg = "Employee: '" + firstName + " " + lastName +
+                    "' updated? '" + result + "'";
+            
+        }
+        
+        return Response.ok(msg, MediaType.TEXT_PLAIN).build();
+        
+    }
+    
+    @DELETE
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("{id: \\d+}")
+    public Response delete(@PathParam("id") int id) {
+        
+        String msg;
+        if(id >= 0) {
+            
+            boolean result = rMgr.deleteEmployee(id);
+            msg = "Employee deleted? '" + result + "'";
+            
+        } else {
+            
+            msg = "Employee not passed in request";
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity(msg).
+                    type(MediaType.TEXT_PLAIN).
+                    build();
+            
+        }
+        
+        return Response.ok(msg, MediaType.TEXT_PLAIN).build();
         
     }
     
